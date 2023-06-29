548c623bbf238b64720ece1f25e06e5889928436
TTigran <tigran.martirosian.1994.2016@gmail.com>
2023-06-29 16:19:49 +0400

    configuration Kafka

---
+++ src/main/java/com/example/springapi/api/controller/KafkaController.java
@@ -0,0 +1,23 @@
+package com.example.springapi.api.controller;
+
+import com.example.springapi.kafka.KafkaProducerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class KafkaController {
+
+    private final KafkaProducerService producerService;
+
+    @Autowired
+    public KafkaController(KafkaProducerService producerService) {
+        this.producerService = producerService;
+    }
+
+    @PostMapping("/messages")
+    public void sendMessage(@RequestBody String message) {
+        producerService.sendMessage(message);
+    }
+}
